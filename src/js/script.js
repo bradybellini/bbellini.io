c2c11bd380ff08ac001273fadad4c697bb71ddad
Brady Bellini <47389606+bradybellini@users.noreply.github.com>
2021-07-23 00:49:42 -0700

    fixed seconds formatting
    added a zero infront of the seconds number when its less than 10

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,46 @@
 var data = [
-    {
-      AboutDevTypeText: "search -g --query "
-    }
-  ];
+  {
+    AboutDevTypeText: "search -g --query ",
+  },
+];
 
-  var allElements = document.getElementsByClassName("typing");
-  for (var j = 0; j < allElements.length; j++) {
-    var currentElementId = allElements[j].id;
-    var currentElementIdContent = data[0][currentElementId];
-    var element = document.getElementById(currentElementId);
-    var devTypeText = currentElementIdContent;
+var allElements = document.getElementsByClassName("typing");
+for (var j = 0; j < allElements.length; j++) {
+  var currentElementId = allElements[j].id;
+  var currentElementIdContent = data[0][currentElementId];
+  var element = document.getElementById(currentElementId);
+  var devTypeText = currentElementIdContent;
 
-    // type code
-    var i = 0, isTag, text;
-    (function type() {
-      text = devTypeText.slice(0, ++i);
-      if (text === devTypeText) return;
-      element.innerHTML = text;
-      var char = text.slice(-1);
-      if (char === "<") isTag = true;
-      if (char === ">") isTag = false;
-      if (isTag) return type();
-      setTimeout(type, 60);
-    })();
-  }
+  // type code
+  var i = 0,
+    isTag,
+    text;
+  (function type() {
+    text = devTypeText.slice(0, ++i);
+    if (text === devTypeText) return;
+    element.innerHTML = text;
+    var char = text.slice(-1);
+    if (char === "<") isTag = true;
+    if (char === ">") isTag = false;
+    if (isTag) return type();
+    setTimeout(type, 60);
+  })();
+}
 
 function updateClock() {
   var now = new Date();
   hours = now.getHours();
-    minutes = now.getMinutes();
-    seconds = now.getSeconds();
-  if (minutes < 10) {
-    time = hours + ":" + "0" + minutes + ":" + seconds;
+  minutes = now.getMinutes();
+  seconds = now.getSeconds();
+  if (seconds < 10) {
+    seconds_ = ":" + "0" + seconds;
   } else {
-    time = hours + ":" + minutes + ":" + seconds;
+    seconds_ = ":" + seconds;
+  }
+  if (minutes < 10) {
+    time = hours + ":" + "0" + minutes + seconds_;
+  } else {
+    time = hours + ":" + minutes + seconds_;
   }
   if (hours < 10) {
     time = "0" + time;
